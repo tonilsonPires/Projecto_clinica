d1090ffc98026b6a19b24d19a75028a965f20a3b
tonilsonPires <tonilsonrijo@gmail.com>
2022-12-24 21:46:20 -0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+
+    
+//Menu responsivo
+    var menuitem = document.getElementById("menuitem");
+    menuitem.style.maxHeight = "0px";
+
+    function menutoggle() {
+      if (menuitem.style.maxHeight == '0px') {
+        menuitem.style .maxHeight = "200px"
+      } else {
+        menuitem.style.maxHeight = "0px"
+      }
+    }
+    
+
+//FINAL DO MENU
+
+
